18df827466e8463eb507bb69702c29baa0fdaa85
skelmis <skelmis.craft@gmail.com>
2020-09-07 01:29:18 +1200

    Implement a finished TicTacToe game featuring: - Implemented a way to get valid computer moves - A computer player (Its bad, its random) - Checks for winners (Win, Loss, Tie) - Parsing User move choices to determine if valid or not

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ Lets make a simple as fuck game, for learning purpose
 1 - X
 2 - O
 """
+import random
+import time
 
 
 def OutputGame(game):
@@ -29,9 +31,150 @@ def OutputGame(game):
         print()
 
 
+def ParseUserInput(userInput):
+    """
+    Given an input, return the row and column selected
+    """
+    if len(userInput) != 3:
+        return False, False
+
+    try:
+        row, column = userInput.split(" ")
+    except ValueError:
+        return False, False
+
+    row = int(row)
+    column = int(column)
+    return row, column
+
+
+def GetValidAIMoves(game):
+    """
+    Iterate over the entire board and create a nested list of valid moves
+    """
+    validMoves = []
+    for rowIndex in range(len(game)):
+        for columnIndex in range(len(game[rowIndex])):
+            if game[rowIndex][columnIndex] == 0:
+                validMoves.append([rowIndex, columnIndex])
+
+    return validMoves
+
+
+def IsBoardFull(game):
+    """
+    Given the game board, figure out if the board is full and thus end the game
+    """
+    for rowIndex in range(len(game)):
+        for columnIndex in range(len(game[rowIndex])):
+            if game[rowIndex][columnIndex] == 0:
+                return False
+
+    return True
+
+
+def HasSomeoneWon(game):
+    """
+    Given the current game state, figure out if someone has won
+    """
+    # First, check all rows
+    for row in game:
+        if all(x == row[0] for x in row):
+            if row[0] == 0:
+                continue
+            return True, row[0]
+
+    # Now lets check all columns
+    currentVerticals = [
+        [game[0][0], game[1][0], game[2][0]],
+        [game[0][1], game[1][1], game[2][1]],
+        [game[0][2], game[1][2], game[2][2]],
+    ]
+    for vertical in currentVerticals:
+        if all(x == vertical[0] for x in vertical):
+            if vertical[0] == 0:
+                continue
+            return True, vertical[0]
+
+    # Lets check diagonals
+    currentDiagonals = [
+        [game[0][0], game[1][1], game[2][2]],
+        [game[0][2], game[1][1], game[2][0]],
+    ]
+    for diagonal in currentDiagonals:
+        if all(x == diagonal[0] for x in diagonal):
+            if diagonal[0] == 0:
+                continue
+            return True, diagonal[0]
+
+    return False, False
+
+
 def main():
-    game = [[x for x in range(3)] for y in range(3)]
-    OutputGame(game)
+    game = [[0 for x in range(3)] for y in range(3)]
+    gameOver = False
+    gameWinner = None
+    userTurn = True
+
+    input(
+        "Welcome to TicTacToe!\nFor your turn, please pick where you want to go based off of the numbers.\nPick the "
+        "row first, and then the column. For example, 0 2, would be the top right of the board.\nPress enter once "
+        "you have finished reading this."
+    )
+    print("\nGame is beginning!\nYou are X\n")
+
+    while not gameOver:
+        OutputGame(game)  # Print the board before anything else
+
+        if userTurn:
+            validInput = False
+            while not validInput:
+                userMove = input("Where do you want to go? ")
+                row, column = ParseUserInput(userMove)
+
+                if row is False:
+                    # Invalid input
+                    print(
+                        "Please provide a valid input!\nPick the row first, and then the column. "
+                        "For example, 0 2, would be the top right of the board."
+                    )
+                    continue
+
+                # Now check if the move itself is legal
+                if game[row][column] != 0:
+                    print("This is not a valid move! Please try again.")
+                    continue
+
+                # Cool, the move is valid. Do it and then move turns
+                game[row][column] = 1
+                userTurn = False
+                validInput = True
+
+        else:
+            # Its the computers turn
+            validMoves = GetValidAIMoves(game)
+            AIMove = random.choice(validMoves)
+            game[AIMove[0]][AIMove[1]] = 2
+            userTurn = True
+            time.sleep(random.random())
+            print(f"\nAI played: {AIMove[0]} {AIMove[1]}")
+
+        boardFull = IsBoardFull(game)
+        isWinner = HasSomeoneWon(game)
+        if isWinner[0] is True:
+            gameWinner = isWinner[1]
+            gameOver = True
+        elif boardFull and isWinner[0] is False:
+            gameWinner = False
+            gameOver = True
+
+    # The game is over, the board is now full or someone has won
+    if gameWinner is False:
+        print("It was a tie!")
+    elif gameWinner == 1:
+        print("Congrats, you won dude!")
+    elif gameWinner == 2:
+        print("Sorry to say you lost dude..")
 
 
 if __name__ == "__main__":
